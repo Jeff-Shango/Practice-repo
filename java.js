8f6670124f3a0b669fca3cbfefbed5adc074f284
Jeff-Shango <105983864+Jeff-Shango@users.noreply.github.com>
2022-06-04 17:31:03 -0400

    new html file
    adding new html file "favorite quotes"
    
    *need to specify the quotes*

--- java.js
+++ java.js
@@ -1,4 +1,4 @@
-const quoteText = document.querySelector(".quote"),
+const quoteText = document.querySelector(".quote"), authorName = document.querySelector(".author")
 quoteBtn = document.querySelector("button");
 
 
@@ -6,6 +6,7 @@ quoteBtn = document.querySelector("button");
         fetch("https://free-quotes-api.herokuapp.com/").then(res => res.json()).then(result =>{
             console.log(result)
             quoteText.innerText = result.quote;
+            authorName.innerText =result.author;
         })
         .catch(err => console.log(err));
 }
